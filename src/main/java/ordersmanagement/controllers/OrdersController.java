fb38bcab8683c91dae8efd3d577e21ba2f95c904
Kilany <y.m.elkilany@gmail.com>
2023-12-26 18:12:08 +0200

    Add example for the project structure

--- src/main/java/ordersmanagement/controllers/OrdersController.java
+++ src/main/java/ordersmanagement/controllers/OrdersController.java
@@ -1,7 +1,16 @@
 package ordersmanagement.controllers;
 
+import ordersmanagement.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("api/orders")
 public class OrdersController {
+
+    private final OrderRepository repository;
+
+    @Autowired
+    public OrdersController(OrderRepository repository) {
+        this.repository = repository;
+    }
 }
